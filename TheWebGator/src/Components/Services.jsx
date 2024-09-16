76d059c11811b8d668ae90ef462c642cb05540a4
MudasirSaeed032 <mudasir.saeed032@gmail.com>
2024-09-16 17:22:33 +0500

    working on services page

--- TheWebGator/src/Components/Services.jsx
+++ TheWebGator/src/Components/Services.jsx
@@ -1,10 +1,40 @@
 import React from 'react'
 import Navbar from './Navbar'
+import QuoteButton from './QuoteButton'
+import webDesign from '../assets/webDesign.png'
+import './Services.css'
 
 export default function Services() {
     return (
         <div>
             <Navbar />
+            <main className='home container-fluid'>
+                <section className='section' id='services-section1'>
+                    <div className='row'>
+                        <div className='col-6 d-flex flex-column justify-content-center text-white'>
+                            <div className='text-white mb-3' id='service-heading'>
+                                Creative Web Design
+                            </div>
+                            <div id='service-quote-button'>
+                                <QuoteButton className='home-quote-button me-auto' />
+                            </div>
+                        </div>
+                        <div className='col-6'>
+                            <img src={webDesign} height={'760px'} alt='web design' />
+                        </div>
+                    </div>
+                </section>
+                <section className='section justify-content-start' id='services-section2'>
+                    <div className='row' id='approach'>
+                        <div className='col-12 text-white h1' id='approach-heading'>
+                            Our Approach to Web Design
+                        </div>
+                        <div className='col-12' style={{ color: '#B8B8B8' }}>
+                            Fusing imagination with practicality
+                        </div>
+                    </div>
+                </section>
+            </main>
         </div>
     )
 }
