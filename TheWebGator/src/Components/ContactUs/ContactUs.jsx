5237c2ffbfb0e8b0ef6ffdf90f5af2589ad5f3e6
MudasirSaeed032 <mudasir.saeed032@gmail.com>
2024-09-26 00:02:26 +0500

    minor changes

--- TheWebGator/src/Components/ContactUs/ContactUs.jsx
+++ TheWebGator/src/Components/ContactUs/ContactUs.jsx
@@ -1,10 +1,20 @@
 import React from 'react'
 import Navbar from '../Boilerplate/Navbar'
+import './ContactUs.css'
 
 export default function ContactUs() {
     return (
         <div>
             <Navbar />
+            <main className='home container'>
+                <section className='section' id='contact-section1'>
+                    <div className='row'>
+                        <div className='col-12 d-flex flex-column justify-content-center text-white'>
+                            Contact our team
+                        </div>
+                    </div>
+                </section>
+            </main>
         </div>
     )
 }
