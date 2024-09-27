08655ce35e8d80b7325e210178f3be1ea3fbe3db
MudasirSaeed032 <mudasir.saeed032@gmail.com>
2024-09-27 23:15:27 +0500

    added contactus.jsx

--- TheWebGator/src/Components/ContactUs/ContactUs.jsx
+++ TheWebGator/src/Components/ContactUs/ContactUs.jsx
@@ -9,8 +9,11 @@ export default function ContactUs() {
             <main className='home container'>
                 <section className='section' id='contact-section1'>
                     <div className='row'>
-                        <div className='col-12 d-flex flex-column justify-content-center text-white'>
+                        <div className='col-12 d-flex flex-row justify-content-center text-white h1'>
                             Contact our team
+                        </div>
+                        <div className='col-12 d-flex flex-row justify-content-center text-gray h5'>
+                            We are here to help you
                         </div>
                     </div>
                 </section>
